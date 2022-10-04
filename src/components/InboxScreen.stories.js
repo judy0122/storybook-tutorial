deb24dee2785b0213edf249ac812b4cb633fff43
jiwon park <jiwon@nextculture.kr>
2022-10-04 11:34:39 +0900

    [storybook] 배포진행중

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -26,16 +26,16 @@ Default.parameters = {
         ]
     }
 }
-Default.play = async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    await waitForElementToBeRemoved(await canvas.findByTestId('loading')).catch((error) => {
-        console.log("error=========", error)
-    })
-    await waitFor(async () => {
-        await fireEvent.click(canvas.getByLabelText('pinTask-1'))
-        await fireEvent.click(canvas.getByLabelText('pinTask-3'))
-    })
-}
+// Default.play = async ({ canvasElement }) => {
+//     const canvas = within(canvasElement)
+//     await waitForElementToBeRemoved(await canvas.findByTestId('loading')).catch((error) => {
+//         console.log("error=========", error)
+//     })
+//     await waitFor(async () => {
+//         await fireEvent.click(canvas.getByLabelText('pinTask-1'))
+//         await fireEvent.click(canvas.getByLabelText('pinTask-3'))
+//     })
+// }
 
 export const Error = Templete.bind({})
 Error.parameters = {
